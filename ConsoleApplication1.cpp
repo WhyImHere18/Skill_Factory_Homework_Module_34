7d8dffa8673d0c8c43277fd255c1865ed547225b
Alexander <42529978+WhyImHere18@users.noreply.github.com>
2023-02-16 21:29:59 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <concepts>
+//#include <bits/stdc++.h>
+#include <string>
+
+template <typename T> concept ComplexConcept = requires(T v)
+{
+    {v.hash()} -> std::convertible_to<long>;
+    {v.toString()} -> std::same_as<std::string>;
+}&& std::has_virtual_destructor<T>::value == false;
+
+struct HashableStruct
+{
+    int num{ 0 };
+    int hash() const { return num << 3; };
+    std::string toString() const { return std::to_string(this->hash()); };
+};
+
+void print(ComplexConcept auto& t)
+{
+    std::cout << t.toString() << std::endl;
+}
+
+int main()
+{
+    HashableStruct num{ 100 };
+    print(num);
+
+    return 0;
+}
